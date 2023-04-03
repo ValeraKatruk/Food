14e37a59f549a5120356686720d3dcebd847ab9c
valerii <walerik19@mail.ru>
2023-04-03 17:28:25 +0800

    CreateClock

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent");
 
+  // функции показ/скрытие контента Tabs
   function hideTabsContent() {
     tabsContent.forEach((element) => {
       element.classList.add("hide");
@@ -35,6 +36,46 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  // Timer
+
+  const deadline = "2023-05-20";
+
+  function getTimeReamaining(endTime) {
+    const t = Date.parse(endTime) - Date.parse(new Date()),
+      days = Math.floor(t / 1000 / 60 / 60 / 24),
+      hours = Math.floor((t / 1000 / 60 / 60) % 24),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function setTimeClock(selector, endTime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeIntelval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      const t = getTimeReamaining(endTime);
+
+      days.textContent = t.days;
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+
+      if (t.total <= 0) clearInterval(timeIntelval);
+    }
+  }
+  setTimeClock(".timer", deadline);
 
   //end DOMContentLoaded
 });
